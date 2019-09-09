c6c21fe0f35f192cd0d8404f5f7e4c35d6a39632
Solomon Padilla <goryfigment@gmail.com>
2019-09-09 16:29:58 -0700

    Home page.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,43 @@
 const webpack = require('webpack');
+const path = require("path");
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleTracker = require('webpack-bundle-tracker');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const glob = require('glob-all');
+const PurifyCSSPlugin = require('purifycss-webpack');
 
 module.exports = {
+    //context: __dirname,
     entry: {
-        home: './templates/src/js/home.js'
+        home: './templates/js/home.js'
     },
-    output: {path: __dirname + '/templates/dist', filename: '[name].bundle.js'},
+    output: {path: __dirname + '/templates/bundle', filename: 'js/[name].js', publicPath: '/templates/bundle/'},
     module: {
         loaders: [
-            {test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader'},
-            {test: /\.(jpe?g|png|gif|svg)$/i, loader: ["file-loader?name=assets/[name].[ext]", 'image-webpack-loader']}
+            {test: /\.css$/, loader: ExtractTextPlugin.extract({use: ['css-loader', 'postcss-loader'], publicPath: '../'})},
+            //{test: /\.(eot|svg|ttf|woff|woff2)$/, loader: 'file-loader?name=assets/fonts/[name].[ext]'},
+            //{test: /\.(jpe?g|png|gif|svg)$/i, loader: ["file-loader?name=../../[path][name].[ext]", 'image-webpack-loader']},
+            {test: /\.hbs$/, loader: 'handlebars-loader', options:{helperDirs: path.resolve(__dirname, "./templates/handlebars/helpers")}}
         ]
     },
     plugins: [
+        new BundleTracker({filename: './webpack-stats.json'}),
+        new ExtractTextPlugin('css/[name].css'),
         new webpack.optimize.CommonsChunkPlugin('vendors'),
-        new UglifyJSPlugin({mangle: {except: ['$super', '$', 'exports', 'require']}}),
-        new HtmlWebpackPlugin({
-            filename: 'home.html',
-            chunks: ['home', 'vendors'],
-            minify: {collapseWhitespace: true},
-            hash: true,
-            template: './templates/src/home.html'
-        })
-    ]
+        new UglifyJSPlugin({mangle: {except: ['$super', '$', 'exports', 'require']}, extractComments: true}),
+
+        //Purify CSS
+        //new PurifyCSSPlugin({paths: glob.sync([path.join(__dirname, 'templates/*.html'), path.join(__dirname, 'templates/partials/*.html')]), minimize: true,
+        //    purifyOptions: {whitelist: []}
+        //}),
+
+        //HTML
+        new HtmlWebpackPlugin({filename: 'home.html', chunks: ['vendors','home'], minify: {collapseWhitespace: true}, hash: true, template: './templates/home.html'})
+    ],
+    resolve: {
+        alias: {
+           handlebars: 'handlebars/dist/handlebars.min.js'
+        }
+    }
 };
